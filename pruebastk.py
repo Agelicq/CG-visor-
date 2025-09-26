58bf6bd25a39f70c2773b3064c6c34dd0b28cba6
angelicaalvarez <angelica.alvarez@utp.edu.co>
2025-09-25 20:28:05 -0500

    first commit

---
+++ pruebastk.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+#redimeniona la ventana 500x1500
+ventana.geometry("500x500")
+
+etiqueta = tk.Label(ventana, text="Hola mundo")
+etiqueta.pack()
+
+#crea la venta 
+ventana.mainloop()
