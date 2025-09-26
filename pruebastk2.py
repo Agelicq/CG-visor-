969ee090ff9e7df79d46f628508599e2d065c428
angelicaalvarez <angelica.alvarez@utp.edu.co>
2025-09-25 20:46:25 -0500

    aaa

--- pruebastk2.py
+++ pruebastk2.py
@@ -1,8 +1,5 @@
-#any change i made here will be reflected in the main branch
-
-print("Hello a ver porque no actualiza aquiii")
-
 import tkinter as tk
+
 
 ventana = tk.Tk()
 #redimeniona la ventana 500x1500
